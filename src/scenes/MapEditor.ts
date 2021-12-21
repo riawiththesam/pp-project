e744e8765b318cb63ae5950bff06dc6e8e4db344
riawiththesam <riawiththesam@gmail.com>
2021-12-22 07:56:31 +0900

    feat: ボタン設定

--- src/scenes/MapEditor.ts
+++ src/scenes/MapEditor.ts
@@ -2,6 +2,7 @@ import Phaser from 'phaser'
 import { MapEditorIntaractor } from '../usecases/mapeditor/MapEditorUseCase'
 import { Map } from '../gameobjects/map/Map'
 import { MapEditorViewModel } from './MapEditorViewModel'
+import { Button } from '../gameobjects/ui/Button'
 
 export default class MapEditorScene extends Phaser.Scene {
   private mapEditorViewModel = new MapEditorViewModel(MapEditorIntaractor)
@@ -12,6 +13,7 @@ export default class MapEditorScene extends Phaser.Scene {
 
   preload () {
     this.load.image('background', '/assets/backgrounds/grass.jpg')
+    this.load.image('button-background', '/assets/ui/pipo-WindowBaseSet2a_02.png')
   }
 
   create () {
@@ -22,5 +24,14 @@ export default class MapEditorScene extends Phaser.Scene {
 
     const mapX = (width / 2) - 100
     this.add.existing(new Map(this, mapX, height / 2, this.mapEditorViewModel))
+
+    const button = new Button({
+      scene: this,
+      x: width / 2 + 300,
+      y: 100,
+      texture: 'button-background',
+      onClick: () => this.mapEditorViewModel.onClickButton()
+    })
+    this.add.existing(button)
   }
 }
--- src/scenes/MapEditorViewModel.ts
+++ src/scenes/MapEditorViewModel.ts
@@ -2,7 +2,12 @@ import { BehaviorSubject } from 'rxjs'
 import { MEMap } from '../usecases/mapeditor/MEMap'
 import { MapEditorUseCase } from '../usecases/mapeditor/MapEditorUseCase'
 
+export type MapEditorEditingSubjectFloor = 'floor'
+export type MapEditorEditingSubjectWall = 'wall'
+export type MapEditorEditingSubject = MapEditorEditingSubjectFloor | MapEditorEditingSubjectWall
+
 export class MapEditorViewModel {
+  public editingSubject = new BehaviorSubject<MapEditorEditingSubject>('floor')
   public map = new BehaviorSubject<MEMap | null>(null)
 
   constructor (
@@ -20,5 +25,17 @@ export class MapEditorViewModel {
     if (!current) return
     const next = current.updateFloor(xIndex, yIndex, 'empty')
     this.map.next(next)
+  }
+
+  onClickButton () {
+    const current = this.editingSubject.value
+    let next: MapEditorEditingSubject = 'floor'
+    if (current === 'floor') {
+      next = 'wall'
+    } else {
+      next = 'floor'
+    }
+    console.log('set ' + next)
+    this.editingSubject.next(next)
   }
 }
