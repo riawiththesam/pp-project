cffe2d06dc60cc3d78ca3593c9c9b1a758b9bf21
riawiththesam <riawiththesam@gmail.com>
2021-12-22 07:56:23 +0900

    feat: ボタンクラス

---
+++ src/gameobjects/ui/Button.ts
@@ -0,0 +1,38 @@
+import Phaser from 'phaser'
+
+type OnClick = () => void
+
+export type PhaserProps = {
+  scene: Phaser.Scene
+  x: number
+  y: number
+}
+
+export type ButtonProps = {
+  texture: string
+  onClick?: OnClick
+} & PhaserProps
+
+export class Button extends Phaser.GameObjects.Container {
+  constructor (
+    props: ButtonProps
+  ) {
+    const {
+      scene,
+      x,
+      y,
+      texture,
+      onClick
+    } = props
+    super(scene, x, y)
+
+    const image = scene.add.image(0, 0, texture)
+    image.setInteractive({ useHandCursor: true })
+
+    if (onClick) {
+      image.on('pointerdown', () => onClick())
+    }
+
+    this.add(image)
+  }
+}
